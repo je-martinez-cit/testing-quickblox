3a0e6ceb2bd140e53d2bcfdb5f97034b797feaa7
Jose Martinez <jose.martinez@cit.hn>
2020-04-30 15:05:12 -0600

    Init

---
+++ src/QBConfig.js
@@ -0,0 +1,8 @@
+export default {
+  appId: '82155',
+  authKey: 'OktX3rATjpaKxZe',
+  authSecret: 'xeW2-RWBhyFhu8r',
+  accountKey: '9dxoEy9S7tyxYArP23Ey',
+  apiEndpoint: '',
+  chatEndpoint: '',
+}
